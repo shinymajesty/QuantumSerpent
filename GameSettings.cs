6cf52c62692577b184bc2d96093d968a3a082728
shinymajesty <rootgives@gmail.com>
2024-04-13 21:51:40 +0200

    #Created static GameSettings class to store game settings

---
+++ GameSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumSerpent
+{
+    static class GameSettings
+    {
+        static int Speed { get; set; } = 166;
+        static string Player1_Name { get; set; } = "Player 1";
+        static string Player2_Name { get; set; } = "Player 2";
+        static bool Bot1 { get; set; } = false;
+        static bool Bot2 { get; set; } = false;
+
+    }
+}
